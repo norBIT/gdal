norBIT/gdal

commit 07191b89346e5c274b2690309bb034e1db27b79d

PDF: fix potential crash with PoDoFo backend when using PDF_DUMP_OBJECT config option
git-svn-id: https://svn.osgeo.org/gdal/trunk@30155 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/pdf/pdfobject.cpp
@@ -1463,7 +1463,7 @@ class GDALPDFStreamPodofo : public GDALPDFStream
 
     public:
         GDALPDFStreamPodofo(PoDoFo::PdfMemStream* pStream) : m_pStream(pStream) { }
-        virtual ~GDALPDFStreamPodofo() {}
+        virtual ~GDALPDFStreamPodofo() { delete m_pStream; }
 
         virtual int GetLength();
         virtual char* GetBytes();
@@ -1673,7 +1673,7 @@ GDALPDFStream* GDALPDFObjectPodofo::GetStream()
     PoDoFo::PdfMemStream* pStream = NULL;
     try
     {
-        pStream = dynamic_cast<PoDoFo::PdfMemStream*>(m_po->GetStream());
+        pStream = new PoDoFo::PdfMemStream(*(dynamic_cast<PoDoFo::PdfMemStream*>(m_po->GetStream())));
         pStream->Uncompress();
     }
     catch( const PoDoFo::PdfError & e )

autotest/gdrivers/pdf.py
@@ -473,7 +473,13 @@ def pdf_rgba_default_compression(options_param = []):
     out_ds = None
 
     #gdal.SetConfigOption('GDAL_PDF_BANDS', '4')
+    gdal.SetConfigOption('PDF_DUMP_OBJECT', 'tmp/rgba.pdf.txt')
+    gdal.SetConfigOption('PDF_DUMP_PARENT', 'YES')
     out_ds = gdal.Open('tmp/rgba.pdf')
+    gdal.SetConfigOption('PDF_DUMP_OBJECT', None)
+    gdal.SetConfigOption('PDF_DUMP_PARENT', None)
+    content = open('tmp/rgba.pdf.txt', 'rt').read()
+    os.unlink('tmp/rgba.pdf.txt')
     cs1 = out_ds.GetRasterBand(1).Checksum()
     cs2 = out_ds.GetRasterBand(2).Checksum()
     cs3 = out_ds.GetRasterBand(3).Checksum()
@@ -493,6 +499,12 @@ def pdf_rgba_default_compression(options_param = []):
 
     if cs4 < 0:
         return 'skip'
+
+    if content.find('Type = dictionary, Num = 3, Gen = 0') != 0 or \
+       content.find('      Type = dictionary, Num = 3, Gen = 0') < 0:
+        gdaltest.post_reason('wrong object dump')
+        print(content)
+        return 'fail'
 
     if cs4 == 0:
         gdaltest.post_reason('wrong checksum')

commit 2ae1d9f47e955742485fa6a8931a7cbdf5b7da9e

PDF: fix breakage on ogr_pdf_4_podofo introduced in r30155
git-svn-id: https://svn.osgeo.org/gdal/trunk@30160 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/pdf/pdfobject.cpp
@@ -1459,11 +1459,11 @@ class GDALPDFArrayPodofo : public GDALPDFArray
 class GDALPDFStreamPodofo : public GDALPDFStream
 {
     private:
-        PoDoFo::PdfMemStream* m_pStream;
+        PoDoFo::PdfStream* m_pStream;
 
     public:
-        GDALPDFStreamPodofo(PoDoFo::PdfMemStream* pStream) : m_pStream(pStream) { }
-        virtual ~GDALPDFStreamPodofo() { delete m_pStream; }
+        GDALPDFStreamPodofo(PoDoFo::PdfStream* pStream) : m_pStream(pStream) { }
+        virtual ~GDALPDFStreamPodofo() { }
 
         virtual int GetLength();
         virtual char* GetBytes();
@@ -1668,26 +1668,9 @@ GDALPDFStream* GDALPDFObjectPodofo::GetStream()
         return NULL;
     }
 
-    if (m_poStream)
-        return m_poStream;
-    PoDoFo::PdfMemStream* pStream = NULL;
-    try
-    {
-        pStream = new PoDoFo::PdfMemStream(*(dynamic_cast<PoDoFo::PdfMemStream*>(m_po->GetStream())));
-        pStream->Uncompress();
-    }
-    catch( const PoDoFo::PdfError & e )
-    {
-        e.PrintErrorMsg();
-        pStream = NULL;
-    }
-    if (pStream)
-    {
-        m_poStream = new GDALPDFStreamPodofo(pStream);
-        return m_poStream;
-    }
-    else
-        return NULL;
+    if (m_poStream == NULL)
+        m_poStream = new GDALPDFStreamPodofo(m_po->GetStream());
+    return m_poStream;
 }
 
 /************************************************************************/
@@ -1829,7 +1812,18 @@ GDALPDFObject* GDALPDFArrayPodofo::Get(int nIndex)
 
 int GDALPDFStreamPodofo::GetLength()
 {
-    return static_cast<int>(m_pStream->GetLength());
+    char* pBuffer = NULL;
+    PoDoFo::pdf_long nLen = 0;
+    try
+    {
+        m_pStream->GetFilteredCopy( &pBuffer, &nLen );
+        PoDoFo::podofo_free(pBuffer);
+        return (int)nLen;
+    }
+    catch( PoDoFo::PdfError & e )
+    {
+    }
+    return 0;
 }
 
 /************************************************************************/
@@ -1838,12 +1832,25 @@ int GDALPDFStreamPodofo::GetLength()
 
 char* GDALPDFStreamPodofo::GetBytes()
 {
-    int nLength = GetLength();
-    char* pszContent = (char*) VSIMalloc(nLength + 1);
-    if (!pszContent)
+    char* pBuffer = NULL;
+    PoDoFo::pdf_long nLen = 0;
+    try
+    {
+        m_pStream->GetFilteredCopy( &pBuffer, &nLen );
+    }
+    catch( PoDoFo::PdfError & e )
+    {
         return NULL;
-    memcpy(pszContent, m_pStream->Get(), nLength);
-    pszContent[nLength] = '\0';
+    }
+    char* pszContent = (char*) VSIMalloc(nLen + 1);
+    if (!pszContent)
+    {
+        PoDoFo::podofo_free(pBuffer);
+        return NULL;
+    }
+    memcpy(pszContent, pBuffer, nLen);
+    PoDoFo::podofo_free(pBuffer);
+    pszContent[nLen] = '\0';
     return pszContent;
 }
 