norBIT/gdal

commit f2c71debe680c15803b880e708aa874d483487d9

Clean frmts/hfa (partial).
- int HFAField::ExtractInstValue -> bool.  And CE_Error returns to false
- Remove redundant casts of size_t to size_t
- C casts -> C++ casts
- Fix formatting
- Localize loop counters

git-svn-id: https://svn.osgeo.org/gdal/trunk@35736 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/hfa/hfa_p.h
@@ -355,16 +355,19 @@ class HFAField
 
     void        Dump( FILE * );
 
-    int         ExtractInstValue( const char * pszField, int nIndexValue,
-                     GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
-                     char chReqType, void *pReqReturn, int *pnRemainingDataSize = NULL );
+    bool        ExtractInstValue( const char * pszField, int nIndexValue,
+                                  GByte *pabyData, GUInt32 nDataOffset,
+                                  int nDataSize, char chReqType,
+                                  void *pReqReturn,
+                                  int *pnRemainingDataSize = NULL );
 
     CPLErr      SetInstValue( const char * pszField, int nIndexValue,
-                     GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
-                     char chReqType, void *pValue );
+                              GByte *pabyData, GUInt32 nDataOffset,
+                              int nDataSize,
+                              char chReqType, void *pValue );
 
-    void        DumpInstValue( FILE *fpOut,
-                               GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
+    void        DumpInstValue( FILE *fpOut, GByte *pabyData,
+                               GUInt32 nDataOffset, int nDataSize,
                                const char *pszPrefix = NULL );
 
     int         GetInstBytes( GByte *, int );
@@ -400,14 +403,15 @@ class HFAType
     void        Dump( FILE * );
 
     int         GetInstBytes( GByte *, int );
-    int         GetInstCount( const char *pszField,
-                          GByte *pabyData, GUInt32 nDataOffset, int nDataSize);
+    int         GetInstCount( const char *pszField, GByte *pabyData,
+                              GUInt32 nDataOffset, int nDataSize );
     int         ExtractInstValue( const char * pszField,
-                                  GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
-                               char chReqType, void *pReqReturn, int *pnRemainingDataSize );
-    CPLErr      SetInstValue( const char * pszField,
-                           GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
-                           char chReqType, void * pValue );
+                                  GByte *pabyData, GUInt32 nDataOffset,
+                                  int nDataSize, char chReqType,
+                                  void *pReqReturn, int *pnRemainingDataSize );
+    CPLErr      SetInstValue( const char * pszField, GByte *pabyData, 
+                              GUInt32 nDataOffset, int nDataSize,
+                              char chReqType, void * pValue );
     void        DumpInstValue( FILE *fpOut,
                            GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
                            const char *pszPrefix = NULL );

gdal/frmts/hfa/hfaband.cpp
@@ -1210,13 +1210,14 @@ CPLErr HFABand::GetRasterBlock( int nXBlock, int nYBlock, void * pData, int nDat
     if( panBlockFlag[iBlock] & BFLG_COMPRESSED )
     {
         GByte *pabyCData = static_cast<GByte *>(
-            VSI_MALLOC_VERBOSE( (size_t) nBlockSize ));
+            VSI_MALLOC_VERBOSE( static_cast<size_t>(nBlockSize) ));
         if( pabyCData == NULL )
         {
             return CE_Failure;
         }
 
-        if( VSIFReadL( pabyCData, (size_t) nBlockSize, 1, fpData ) != 1 )
+        if( VSIFReadL( pabyCData, static_cast<size_t>(nBlockSize),
+                       1, fpData ) != 1 )
         {
             CPLFree( pabyCData );
 
@@ -1478,16 +1479,17 @@ CPLErr HFABand::SetRasterBlock( int nXBlock, int nYBlock, void * pData )
 #endif  // def CPL_MSB
 
             // Write out the Minimum value.
-            bool bRet = VSIFWriteL( &nMin, (size_t) sizeof( nMin ), 1, fpData ) > 0;
+            bool bRet = VSIFWriteL( &nMin, sizeof( nMin ), 1, fpData ) > 0;
 
             // The number of runs.
-            bRet &= VSIFWriteL( &nNumRuns, (size_t) sizeof( nNumRuns ), 1, fpData ) > 0;
+            bRet &= VSIFWriteL( &nNumRuns, sizeof( nNumRuns ), 1, fpData ) > 0;
 
             // The offset to the data.
-            bRet &= VSIFWriteL( &nDataOffset, (size_t) sizeof( nDataOffset ), 1, fpData ) > 0;
+            bRet &= VSIFWriteL( &nDataOffset, sizeof( nDataOffset ),
+                                1, fpData ) > 0;
 
             // The number of bits.
-            bRet &= VSIFWriteL( &nNumBits, (size_t) sizeof( nNumBits ), 1, fpData ) > 0;
+            bRet &= VSIFWriteL( &nNumBits, sizeof( nNumBits ), 1, fpData ) > 0;
 
             // The counters - MSB stuff handled in HFACompress.
             bRet &= VSIFWriteL( pCounts, nSizeCount, 1, fpData ) > 0;
@@ -1599,7 +1601,8 @@ CPLErr HFABand::SetRasterBlock( int nXBlock, int nYBlock, void * pData )
 /* -------------------------------------------------------------------- */
 /*      Write uncompressed data.                                        */
 /* -------------------------------------------------------------------- */
-        if( VSIFWriteL( pData, (size_t) nBlockSize, 1, fpData ) != 1 )
+        if( VSIFWriteL( pData, static_cast<size_t>(nBlockSize),
+                        1, fpData ) != 1 )
         {
             CPLError( CE_Failure, CPLE_FileIO,
                       "Write of %d bytes at %x:%08x on %p failed.\n%s",
@@ -1888,7 +1891,7 @@ CPLErr HFABand::GetPCT( int * pnColors,
                     return CE_Failure;
                 }
                 if( VSIFReadL( apadfPCT[iColumn], sizeof(double), nPCTColors,
-                               psInfo->fp) != (size_t)nPCTColors )
+                               psInfo->fp) != static_cast<size_t>(nPCTColors) )
                 {
                     CPLError( CE_Failure, CPLE_FileIO,
                               "VSIFReadL() failed in HFABand::GetPCT()." );

commit 1236ae0f3cdf4d60d9e7724970415cc3ab3d14d2

Fix HFA driver ExtractInstValue broken by r35736.
Make all be methods that return bool.

Also
- Fix formatting
- Add const to methods
- /* */ -> //
- Fit to 80 cols
- C casts -> C++ casts

git-svn-id: https://svn.osgeo.org/gdal/trunk@35737 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/hfa/hfa.h
@@ -193,9 +193,9 @@ CPLErr CPL_DLL HFAGetRasterBlockEx( HFAHandle hHFA, int nBand, int nXBlock,
 CPLErr CPL_DLL HFAGetOverviewRasterBlock( HFAHandle hHFA, int nBand,
                                           int iOverview,
                                    int nXBlock, int nYBlock, void * pData );
-CPLErr CPL_DLL HFAGetOverviewRasterBlockEx( HFAHandle hHFA, int nBand,
-                                          int iOverview,
-                                   int nXBlock, int nYBlock, void * pData, int nDataSize );
+CPLErr CPL_DLL HFAGetOverviewRasterBlockEx(
+                   HFAHandle hHFA, int nBand, int iOverview,
+                   int nXBlock, int nYBlock, void * pData, int nDataSize );
 CPLErr CPL_DLL HFASetRasterBlock( HFAHandle hHFA, int nBand,
                                   int nXBlock, int nYBlock,
                                   void * pData );

gdal/frmts/hfa/hfa_p.h
@@ -69,8 +69,8 @@ struct hfainfo {
     VSILFILE    *fp;
 
     char        *pszPath;
-    char        *pszFilename; /* sans path */
-    char        *pszIGEFilename; /* sans path */
+    char        *pszFilename;  // Sans path.
+    char        *pszIGEFilename;  // Sans path.
 
     HFAAccess   eAccess;
 
@@ -143,12 +143,12 @@ class HFABand
 {
     int         nBlocks;
 
-    // Used for single-file modification
+    // Used for single-file modification.
     vsi_l_offset *panBlockStart;
     int         *panBlockSize;
     int         *panBlockFlag;
 
-    // Used for spill-file modification
+    // Used for spill-file modification.
     vsi_l_offset nBlockStart;
     vsi_l_offset nBlockSize;
     int         nLayerStackCount;
@@ -196,7 +196,8 @@ class HFABand
     int         nOverviews;
     HFABand     **papoOverviews;
 
-    CPLErr      GetRasterBlock( int nXBlock, int nYBlock, void * pData, int nDataSize );
+    CPLErr      GetRasterBlock( int nXBlock, int nYBlock, void * pData,
+                                int nDataSize );
     CPLErr      SetRasterBlock( int nXBlock, int nYBlock, void * pData );
 
     const char * GetBandName();
@@ -266,9 +267,10 @@ class HFAEntry
 
 public:
     static HFAEntry* New( HFAInfo_t * psHFA, GUInt32 nPos,
-                          HFAEntry * poParent, HFAEntry *poPrev) CPL_WARN_UNUSED_RESULT;
+                          HFAEntry * poParent,
+                          HFAEntry *poPrev) CPL_WARN_UNUSED_RESULT;
 
-                 HFAEntry( HFAInfo_t *psHFA,
+                HFAEntry( HFAInfo_t *psHFA,
                           const char *pszNodeName,
                           const char *pszTypeName,
                           HFAEntry *poParent );
@@ -280,12 +282,13 @@ public:
 
     virtual     ~HFAEntry();
 
-    static HFAEntry*  BuildEntryFromMIFObject( HFAEntry *poContainer,
-                                               const char *pszMIFObjectPath ) CPL_WARN_UNUSED_RESULT;
+    static HFAEntry*  BuildEntryFromMIFObject(
+                          HFAEntry *poContainer,
+                          const char *pszMIFObjectPath ) CPL_WARN_UNUSED_RESULT;
 
     CPLErr      RemoveAndDestroy();
 
-    GUInt32     GetFilePos() CPL_WARN_UNUSED_RESULT { return nFilePos; }
+    GUInt32     GetFilePos() const CPL_WARN_UNUSED_RESULT { return nFilePos; }
 
     const char  *GetName() CPL_WARN_UNUSED_RESULT { return szName; }
     void SetName( const char *pszNodeName );
@@ -294,8 +297,8 @@ public:
     HFAType     *GetTypeObject() CPL_WARN_UNUSED_RESULT;
 
     GByte      *GetData() CPL_WARN_UNUSED_RESULT { LoadData(); return pabyData; }
-    GUInt32     GetDataPos() CPL_WARN_UNUSED_RESULT { return nDataPos; }
-    GUInt32     GetDataSize() CPL_WARN_UNUSED_RESULT { return nDataSize; }
+    GUInt32     GetDataPos() const CPL_WARN_UNUSED_RESULT { return nDataPos; }
+    GUInt32     GetDataSize() const CPL_WARN_UNUSED_RESULT { return nDataSize; }
 
     HFAEntry    *GetChild() CPL_WARN_UNUSED_RESULT;
     HFAEntry    *GetNext() CPL_WARN_UNUSED_RESULT;
@@ -334,17 +337,17 @@ class HFAField
     int         nBytes;
 
     int         nItemCount;
-    char        chPointer;      /* '\0', '*' or 'p' */
-    char        chItemType;     /* 1|2|4|e|... */
+    char        chPointer;      // '\0', '*' or 'p'
+    char        chItemType;     // 1|2|4|e|...
 
-    char        *pszItemObjectType; /* if chItemType == 'o' */
+    char        *pszItemObjectType;  // if chItemType == 'o'
     HFAType     *poItemObjectType;
 
-    char        **papszEnumNames; /* normally NULL if not an enum */
+    char        **papszEnumNames;  // Normally NULL if not an enum.
 
     char        *pszFieldName;
 
-    char        szNumberString[36]; /* buffer used to return an int as a string */
+    char        szNumberString[36];  // Buffer used to return int as a string.
 
                 HFAField();
                 ~HFAField();
@@ -405,16 +408,16 @@ class HFAType
     int         GetInstBytes( GByte *, int );
     int         GetInstCount( const char *pszField, GByte *pabyData,
                               GUInt32 nDataOffset, int nDataSize );
-    int         ExtractInstValue( const char * pszField,
+    bool        ExtractInstValue( const char * pszField,
                                   GByte *pabyData, GUInt32 nDataOffset,
                                   int nDataSize, char chReqType,
                                   void *pReqReturn, int *pnRemainingDataSize );
     CPLErr      SetInstValue( const char * pszField, GByte *pabyData, 
                               GUInt32 nDataOffset, int nDataSize,
                               char chReqType, void * pValue );
-    void        DumpInstValue( FILE *fpOut,
-                           GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
-                           const char *pszPrefix = NULL );
+    void        DumpInstValue( FILE *fpOut, GByte *pabyData, 
+                               GUInt32 nDataOffset, int nDataSize,
+                               const char *pszPrefix = NULL );
 };
 
 /************************************************************************/
@@ -462,13 +465,13 @@ public:
   static bool QueryDataTypeSupported( EPTType eHFADataType );
 
   // Get methods - only valid after compressBlock has been called.
-  GByte*  getCounts()     { return m_pCounts; };
-  GUInt32 getCountSize()  { return m_nSizeCounts; };
-  GByte*  getValues()     { return m_pValues; };
-  GUInt32 getValueSize()  { return m_nSizeValues; };
-  GUInt32 getMin()        { return m_nMin; };
-  GUInt32 getNumRuns()    { return m_nNumRuns; };
-  GByte   getNumBits()    { return m_nNumBits; };
+  GByte*  getCounts() const { return m_pCounts; };
+  GUInt32 getCountSize() const { return m_nSizeCounts; };
+  GByte*  getValues() const { return m_pValues; };
+  GUInt32 getValueSize() const { return m_nSizeValues; };
+  GUInt32 getMin() const { return m_nMin; };
+  GUInt32 getNumRuns() const { return m_nNumRuns; };
+  GByte   getNumBits() const { return m_nNumBits; };
 
 private:
   void makeCount( GUInt32 count, GByte *pCounter, GUInt32 *pnSizeCount );

gdal/frmts/hfa/hfaband.cpp
@@ -215,7 +215,7 @@ CPLErr HFABand::LoadOverviews()
             if( pszPath[strlen(pszPath)-1] == ')' )
                 pszPath[strlen(pszPath)-1] = '\0';
 
-            for( int i=0; pszPath[i] != '\0'; i++ )
+            for( int i = 0; pszPath[i] != '\0'; i++ )
             {
                 if( pszPath[i] == ':' )
                     pszPath[i] = '.';
@@ -505,8 +505,8 @@ CPLErr HFABand::LoadExternalBlockInfo()
 /*      Validity is determined from the validity bitmap.                */
 /* -------------------------------------------------------------------- */
     nBlockStart = poDMS->GetBigIntField( "layerStackDataOffset" );
-    nBlockSize = (nBlockXSize*static_cast<vsi_l_offset>(nBlockYSize)
-                  *HFAGetDataTypeBits(eDataType)+7) / 8;
+    nBlockSize = (nBlockXSize * static_cast<vsi_l_offset>(nBlockYSize)
+                  *HFAGetDataTypeBits(eDataType) + 7) / 8;
 
     for( int iBlock = 0; iBlock < nBlocks; iBlock++ )
     {
@@ -597,7 +597,7 @@ static CPLErr UncompressBlock( GByte *pabyCData, int nSrcBytes,
 
             if( nNumBits == 0 )
             {
-                nRawValue = 0;
+                // nRawValue = 0;
             }
             else if( nNumBits == 1 )
             {
@@ -746,8 +746,9 @@ static CPLErr UncompressBlock( GByte *pabyCData, int nSrcBytes,
          nNumBits * nNumRuns > INT_MAX - 7 ||
          (nNumBits * nNumRuns + 7)/8 > INT_MAX - nDataOffset) )
     {
-        CPLError(CE_Failure, CPLE_AppDefined,
-                 "Integer overflow : nDataOffset + (nNumBits * nNumRuns + 7)/8");
+        CPLError(
+            CE_Failure, CPLE_AppDefined,
+            "Integer overflow : nDataOffset + (nNumBits * nNumRuns + 7)/8");
         return CE_Failure;
     }
     CHECK_ENOUGH_BYTES(nDataOffset + (nNumBits * nNumRuns + 7)/8);
@@ -773,7 +774,7 @@ static CPLErr UncompressBlock( GByte *pabyCData, int nSrcBytes,
         if( ((*pabyCounter) & 0xc0) == 0x00 )
         {
             nRepeatCount = (*(pabyCounter++)) & 0x3f;
-            nCounterOffset ++;
+            nCounterOffset++;
         }
         else if( ((*pabyCounter) & 0xc0) == 0x40 )
         {
@@ -808,7 +809,7 @@ static CPLErr UncompressBlock( GByte *pabyCData, int nSrcBytes,
 
         if( nNumBits == 0 )
         {
-            nDataValue = 0;
+            // nDataValue = 0;
         }
         else if( nNumBits == 1 )
         {
@@ -1030,7 +1031,7 @@ void HFABand::NullBlock( void *pData )
         {
           case EPT_u1:
           {
-              nWords = (nWords + 7)/8;
+              nWords = (nWords + 7) / 8;
               if( dfNoData != 0.0 )
                   ((unsigned char *) abyTmp)[0] = 0xff;
               else
@@ -1040,7 +1041,7 @@ void HFABand::NullBlock( void *pData )
 
           case EPT_u2:
           {
-              nWords = (nWords + 3)/4;
+              nWords = (nWords + 3) / 4;
               if( dfNoData == 0.0 )
                   ((unsigned char *) abyTmp)[0] = 0x00;
               else if( dfNoData == 1.0 )
@@ -1054,10 +1055,10 @@ void HFABand::NullBlock( void *pData )
 
           case EPT_u4:
           {
-              unsigned char byVal = static_cast<unsigned char>(
+              const unsigned char byVal = static_cast<unsigned char>(
                   MAX(0, MIN(15, static_cast<int>(dfNoData))));
 
-              nWords = (nWords + 1)/2;
+              nWords = (nWords + 1) / 2;
 
               ((unsigned char *) abyTmp)[0] = byVal + (byVal << 4);
           }
@@ -1143,7 +1144,8 @@ void HFABand::NullBlock( void *pData )
 /*                           GetRasterBlock()                           */
 /************************************************************************/
 
-CPLErr HFABand::GetRasterBlock( int nXBlock, int nYBlock, void * pData, int nDataSize )
+CPLErr HFABand::GetRasterBlock( int nXBlock, int nYBlock,
+                                void * pData, int nDataSize )
 
 {
     if( LoadBlockInfo() != CE_None )
@@ -1871,7 +1873,8 @@ CPLErr HFABand::GetPCT( int * pnColors,
             }
             else if( iColumn == 3 )
             {
-                poColumnEntry = poNode->GetNamedChild("Descriptor_Table.Opacity");
+                poColumnEntry =
+                    poNode->GetNamedChild("Descriptor_Table.Opacity");
             }
 
             if( poColumnEntry == NULL )

gdal/frmts/hfa/hfafield.cpp
@@ -1257,12 +1257,13 @@ HFAField::ExtractInstValue( const char * pszField, int nIndexValue,
 
             if( pszField != NULL && strlen(pszField) > 0 )
             {
-                return poItemObjectType->
-                    ExtractInstValue( pszField, pabyRawData,
-                                      nDataOffset + nExtraOffset,
-                                      nDataSize - nExtraOffset,
-                                      chReqType, pReqReturn,
-                                      pnRemainingDataSize );
+                return
+                    poItemObjectType->
+                        ExtractInstValue( pszField, pabyRawData,
+                                          nDataOffset + nExtraOffset,
+                                          nDataSize - nExtraOffset,
+                                          chReqType, pReqReturn,
+                                          pnRemainingDataSize );
             }
         }
         break;
@@ -1505,17 +1506,17 @@ void HFAField::DumpInstValue( FILE *fpOut,
     if( chItemType == 'b' )
     {
         int nDataType = 0;
-        const bool bSuccess = CPL_TO_BOOL(
+        const bool bSuccess =
             ExtractInstValue( NULL, -3, pabyData, nDataOffset,
-                              nDataSize, 'i', &nDataType ));
+                              nDataSize, 'i', &nDataType );
         if( bSuccess )
         {
             int nColumns = 0;
             ExtractInstValue( NULL, -2, pabyData, nDataOffset,
-                            nDataSize, 'i', &nColumns );
+                              nDataSize, 'i', &nColumns );
             int nRows = 0;
             ExtractInstValue( NULL, -1, pabyData, nDataOffset,
-                            nDataSize, 'i', &nRows );
+                              nDataSize, 'i', &nRows );
             CPL_IGNORE_RET_VAL(VSIFPrintf(
                 fpOut, "%sBASEDATA(%s): %dx%d of %s\n",
                 pszPrefix, pszFieldName,

gdal/frmts/hfa/hfatype.cpp
@@ -99,8 +99,8 @@ const char *HFAType::Initialize( const char * pszInput )
         pszInput = poNewField->Initialize( pszInput );
         if( pszInput != NULL )
         {
-            papoFields = (HFAField **)
-                CPLRealloc(papoFields, sizeof(void*) * (nFields+1) );
+            papoFields = static_cast<HFAField **>(
+                CPLRealloc(papoFields, sizeof(void*) * (nFields + 1) ));
             papoFields[nFields++] = poNewField;
         }
         else
@@ -369,7 +369,7 @@ HFAType::GetInstCount( const char * pszFieldPath,
 /*                                                the third abc struct. */
 /************************************************************************/
 
-int
+bool
 HFAType::ExtractInstValue( const char * pszFieldPath,
                            GByte *pabyData, GUInt32 nDataOffset, int nDataSize,
                            char chReqType, void *pReqReturn,
@@ -434,14 +434,14 @@ HFAType::ExtractInstValue( const char * pszFieldPath,
         if( nInc < 0 || nByteOffset > INT_MAX - nInc )
         {
             CPLError(CE_Failure, CPLE_AppDefined, "Invalid return value");
-            return FALSE;
+            return false;
         }
 
         nByteOffset += nInc;
     }
 
     if( iField == nFields || nByteOffset >= nDataSize )
-        return FALSE;
+        return false;
 
 /* -------------------------------------------------------------------- */
 /*      Extract this field value, and return.                           */